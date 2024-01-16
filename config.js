dbcfbf48b29fa2b582de2183f217c18622f2bff5
Shahid Mangal <shahidmangal21303@gmail.com>
2024-01-17 00:00:08 +0500

    1

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT = 5000;
+
+export const mongoDBURL =  'mongodb://localhost:27017/shahid';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
